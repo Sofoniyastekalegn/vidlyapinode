2ecc083504f1e3ceb634d184217130794d5e5eb9
Sofoniyastekalegn <sofoniyastekalegn@gmail.com>
2024-10-23 08:50:02 +0300

    movie

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
-const express = require('express');
-const { resolve } = require('path');
-const Joi = require('joi');
-const app = express();
-app.use(express.json());
-
+const winston = require("winston");
+const express = require("express");
+const config = require("config");
 const app = express();
 
-const courses = [
-  { id: 1, name: 'course1' },
-  { id: 2, name: 'course2' },
-  { id: 3, name: 'course3' },
-];
+require("./startup/logging")();
+require("./startup/cors")(app);
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/config")();
+require("./startup/validation")();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+const port = process.env.PORT || config.get("port");
+const server = app.listen(port, () =>
+  winston.info(`Listening on port ${port}...`)
+);
 
-app.get('/api/courses', (req, res) => {
-  res.send(courses);
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+module.exports = server;
